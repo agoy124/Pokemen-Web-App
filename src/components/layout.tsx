b3874a1e9565cbebb2c8661e74a1d62ca72bac29
JDT16 Prayoga Hajid Mu'arif <jdt16.prayoga.muarif@gmail.com>
2025-09-29 21:29:09 +0700

    fix color

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Layout = (props: Readonly<Props>) => {
   const { children } = props;
   return (
-    <main className="flex justify-center bg-slate-900 h-screen">
+    <main className="flex justify-center bg-amber-300 h-screen">
       <div className="layout-container min-w-full bg-amber-300 max-w-full dark:bg-neutral-800 md:min-w-[480px] md:max-w-[480px] flex flex-col">
         <Header />
         <div className="flex-grow overflow-auto scrollbar-hidden">{children}</div>
